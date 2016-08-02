2952b72251e5493be8fa61785692c8415d3d815d
Guillaume WOODWARD <gwoodwar@e3r4p15.42.fr>
2016-08-02 15:34:56 +0200

    change mmap args need to be sure

--- srcs/malloc_reg.c
+++ srcs/malloc_reg.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <gwoodwar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/29 18:02:13 by gwoodwar          #+#    #+#             */
-/*   Updated: 2016/08/02 15:26:26 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2016/08/02 15:32:57 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,6 @@
 #include "malloc.h"
 
 #define BLOCK_SIZE(size)	(size + META_SIZE)
-
-static t_chunk		*add_chunk(t_zone *zone)
-{
-	t_chunk				*new;
-	t_block				*first_block;
-
-	new = mmap(0, zone->r_size, MMAP_PROT, MMAP_FLAG);
-	new->h_block = DLST_INIT(&new->h_block);
-	new->remain_size = zone->r_size - CHUNK_SIZE;
-	first_block = new + CHUNK_SIZE;
-	first_block = DLST_INIT(&first_block->h_dlst);
-	first_block->size = new->remain_size - META_SIZE;
-	first_block->free = true;
-	dlst_add_tail(first_block, new->h_block);
-	dlst_add_tail(new, zone->head);
-	return (new);
-}
-
-static t_chunk		*find_chunk(t_zone *zone, size_t size)
-{
-	t_dlst				*it;
-	t_chunk				*chunk;
-
-	it = zone->head;
-	while ((it = it->next) != zone->head)
-	{
-		chunk = C_NODE(t_chunk,it);
-		if (chunk->remain_size >= size + META_SIZE)
-			return (chunk);
-	}
-	return (add_chunk(zone));
-}
 
 static t_block		*split_block(t_chunk *chunk, t_block *free_block,
 									size_t size)
@@ -76,6 +44,40 @@ static t_block		*find_block(t_chunk *chunk, size_t size);
 	}
 	return (NULL);
 }
+
+static t_chunk		*add_chunk(t_zone *zone)
+{
+	t_chunk				*new;
+	t_block				*first_block;
+
+	if (!(new = mmap(0, zone->r_size, MMAP_PROT, MMAP_FLAG, -1, 0)))
+		return (NULL);
+	new->h_block = DLST_INIT(&new->h_block);
+	new->remain_size = zone->r_size - CHUNK_SIZE;
+	first_block = new + CHUNK_SIZE;
+	first_block = DLST_INIT(&first_block->h_dlst);
+	first_block->size = new->remain_size - META_SIZE;
+	first_block->free = true;
+	dlst_add_tail(first_block, new->h_block);
+	dlst_add_tail(new, zone->head);
+	return (new);
+}
+
+static t_chunk		*find_chunk(t_zone *zone, size_t size)
+{
+	t_dlst				*it;
+	t_chunk				*chunk;
+
+	it = zone->head;
+	while ((it = it->next) != zone->head)
+	{
+		chunk = C_NODE(t_chunk,it);
+		if (chunk->remain_size >= size + META_SIZE)
+			return (chunk);
+	}
+	return (add_chunk(zone));
+}
+
 
 void				*malloc_reg(size_t size, t_zone *zone)
 {
